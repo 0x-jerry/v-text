7f83b45e7f9d400282b34f135e8aa467736a98e1
0x-jerry <x.jerry.wang@gmail.com>
2021-08-26 16:36:07 +0800

    chore: update resolver

--- vite-components-resolver.js
+++ vite-components-resolver.js
@@ -3,9 +3,10 @@ import { ComponentResolver } from 'vite-plugin-components'
 /**
  * Resolver for V-Text
  *
+ * @returns {ComponentResolver}
  */
-export function VTextResolver(): ComponentResolver {
-  return (name: string) => {
+export function VTextResolver() {
+  return (name) => {
     if (name.match(/^T[A-Z]/))
       return {
         path: `@0x-jerry/v-text/src/${name}.vue`,
